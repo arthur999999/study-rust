038802030e2df13757069f63818268f817ac4de0
arthur999999 <arthurhono.contato@gmail.com>
2024-10-28 21:43:47 -0300

    test ping message again

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use std::{
 };
 
 use serde::{Deserialize, Serialize};
-use solana_bloom::bloom::Bloom;
 use solana_gossip::{
     cluster_info,
     contact_info::{self, ContactInfo},
@@ -13,12 +12,12 @@ use solana_gossip::{
     ping_pong::{self, Ping, Pong},
 };
 use solana_sdk::{
-    hash::Hash,
     pubkey::Pubkey,
     signature::{Keypair, Signature},
     signer::Signer,
     timing::timestamp,
 };
+use solana_streamer::socket::SocketAddrSpace;
 
 //dont work
 
@@ -34,7 +33,8 @@ fn main() -> std::io::Result<()> {
 
     let keypair = Keypair::new();
 
-    let contact_info = ContactInfo::new_gossip_entry_point(&my_ip);
+    let mut contact_info = ContactInfo::new(keypair.pubkey(), timestamp(), 0);
+    let set_gossip = contact_info.set_gossip(my_ip);
 
     let value = CrdsValue::new_signed(CrdsData::ContactInfo(contact_info), &keypair);
 
@@ -42,28 +42,9 @@ fn main() -> std::io::Result<()> {
 
     let ping_message = Ping::new([2_u8; 32], &keypair).expect("failed creat ping");
 
-    let message2 = Protocol::PingMessage(ping_message);
+    let message = Protocol::PingMessage(ping_message);
 
-    let message = Protocol::PullRequest(filter, value);
-
-    let serealized = bincode::serialize(&message2).expect("Failed bincode");
-
-    let result_send = socket.send_to(&serealized, solana_addr);
-
-    println!("result send {:?}", result_send);
-
-    let recive_message = listen_for_gossip_messages(&socket);
-
-    match recive_message {
-        Some(message) => {
-            let protocol: Protocol = bincode::deserialize(&message).expect("Failed deserialize");
-
-            println!("Protocol {:?}", protocol);
-        }
-        None => {
-            println!("No message recived");
-        }
-    }
+    // let message = Protocol::PullRequest(filter, value);
 
     let serealized = bincode::serialize(&message).expect("Failed bincode");
 
@@ -134,41 +115,3 @@ pub struct PruneData {
     /// Wallclock of the node that generated this message
     wallclock: u64,
 }
-
-// #[derive(Serialize, Deserialize, Clone, PartialEq, Eq, Debug)]
-// pub struct CrdsFilter {
-//     pub filter: Bloom<Hash>,
-//     mask: u64,
-//     mask_bits: u32,
-// }
-
-// impl Default for CrdsFilter {
-//     fn default() -> Self {
-//         fn compute_mask(seed: u64, mask_bits: u32) -> u64 {
-//             assert!(seed <= 2u64.pow(mask_bits));
-//             let seed: u64 = seed.checked_shl(64 - mask_bits).unwrap_or(0x0);
-//             seed | (!0u64).checked_shr(mask_bits).unwrap_or(!0x0)
-//         }
-
-//         #[allow(clippy::cast_possible_truncation)]
-//         #[allow(clippy::cast_sign_loss)]
-//         fn mask_bits(num_items: f64, max_items: f64) -> u32 {
-//             // for small ratios this can result in a negative number, ensure it returns 0 instead
-//             ((num_items / max_items).log2().ceil()).max(0.0) as u32
-//         }
-
-//         let max_items: u32 = 1287;
-//         let num_items: u32 = 512;
-//         let false_rate: f64 = 0.1f64;
-//         let max_bits = 7424u32;
-//         let mask_bits = mask_bits(f64::from(num_items), f64::from(max_items));
-
-//         let bloom: Bloom<Hash> = Bloom::random(max_items as usize, false_rate, max_bits as usize);
-
-//         CrdsFilter {
-//             filter: bloom,
-//             mask: compute_mask(0_u64, mask_bits),
-//             mask_bits,
-//         }
-//     }
-// }
